778e8bf479e6aec1f10088424f71d96dd8b7b3de
Denys Fernandes <38758938+ParagonVirtuoso@users.noreply.github.com>
2023-01-19 23:58:39 -0300

    chore/modal-mesma-resposta
    Correção tamanho dos subcompontes da tela de teste.

--- lib/screens/homePage/home_page.dart
+++ lib/screens/homePage/home_page.dart
@@ -101,7 +101,7 @@ class _HomePageState extends State<HomePage> {
                     ],
                   )),
               Container(
-                height: 1338.h,
+                height: 1325.h,
                 width: 1080.w,
                 padding: EdgeInsets.only(top: 50.h, left: 100.w, right: 100.w),
                 decoration: const BoxDecoration(
--- lib/screens/questionsPage/question_page.dart
+++ lib/screens/questionsPage/question_page.dart
@@ -26,155 +26,168 @@ class _QuestionsPageState extends State<QuestionsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        toolbarHeight: 0,
-        systemOverlayStyle: const SystemUiOverlayStyle(
-          statusBarColor: Colors.transparent,
-          statusBarIconBrightness: Brightness.dark,
-        ),
-        iconTheme: const IconThemeData(color: Colors.white),
-      ),
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Container(
-              width: 50.w,
-              height: 45.h,
-              margin: EdgeInsets.only(left: 40.w, top: 30.h),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Cores.kWhiteColor,
-                borderRadius: BorderRadius.circular(13.r),
-                border: Border.all(color: Cores.kTertiaryColor, width: 4.w),
-              ),
-              child: const Icon(
-                Icons.arrow_back_ios_new,
-                color: Cores.kTertiaryColor,
-                size: 13,
-              ),
-            ),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          toolbarHeight: 0,
+          systemOverlayStyle: const SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            statusBarIconBrightness: Brightness.dark,
           ),
-          Container(
-            width: 900.w,
-            margin: EdgeInsets.only(top: 30.h, right: 20.w),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  '${Strings.etapa} ${currentStep + 1}',
-                  style: TextStyle(
-                      fontSize: 40.sp,
-                      fontFamily: 'Roboto-Regular',
-                      fontWeight: FontWeight.w500),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 30.h),
-                  height: 130.h,
-                  alignment: Alignment.center,
-                  child: Text(
-                    etapas[currentStep],
-                    style: TextStyle(
-                        fontSize: 60.sp,
-                        fontFamily: 'Roboto-Regular',
-                        fontWeight: FontWeight.w500),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 30.h),
-                  child: Image.asset(
-                    Strings.pergunta1Asset,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 30.h),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Cores.kTertiaryColor, width: 4.w),
-                    borderRadius: BorderRadius.circular(13.r),
-                  ),
-                  child: LinearProgressIndicator(
-                    value: progressoTotal,
-                    backgroundColor: Cores.kWhiteColor,
-                    valueColor: const AlwaysStoppedAnimation<Color>(
-                        Cores.kTertiaryColor),
-                  ),
-                ),
-                Text('${progressoTotal.toStringAsFixed(2)}% completo',
-                    style: TextStyle(
-                        fontSize: 33.sp,
-                        fontFamily: 'Roboto-Regular',
-                        fontWeight: FontWeight.w500,
-                        color: Cores.kTertiaryColor)),
-                Container(
-                  margin: EdgeInsets.only(top: 30.h),
-                  height: 130.h,
-                  alignment: Alignment.center,
-                  child: Text(
-                    perguntas[currentStep][currentQuestion],
-                    style: TextStyle(
-                        fontSize: 60.sp,
-                        fontFamily: 'Roboto-Regular',
-                        fontWeight: FontWeight.w500),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 30.h),
-                  child: Text(
-                    Strings.quantEstrelaGosta,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 45.sp, fontFamily: 'Roboto-Regular'),
-                  ),
-                ),
-                StarRate(
-                  selectedStar: selectedStar,
-                  selecionarEstrela: selecionarEstrela,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    registrarResposta();
-                  },
-                  child: Container(
-                    height: 130.h,
-                    width: 350.w,
-                    margin: EdgeInsets.only(top: 5.h, bottom: 5.h),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30.r),
-                        color: corBotaoNext),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Image(
-                            image: AssetImage(Strings.proximoIconAsset)),
-                        SizedBox(
-                          width: 20.w,
-                        ),
-                        Text(
-                          Strings.proximo,
-                          style: TextStyle(
-                            fontSize: 45.sp,
-                            color: Cores.kWhiteColor,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ],
+          iconTheme: const IconThemeData(color: Colors.white),
+        ),
+        body: Container(
+          margin: EdgeInsets.only(left: 50.w, right: 50.w, top: 10.h, bottom: 10.h),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      width: 50.w,
+                      height: 45.h,
+                      margin: EdgeInsets.only(left: 40.w, top: 20.h),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Cores.kWhiteColor,
+                        borderRadius: BorderRadius.circular(13.r),
+                        border:
+                        Border.all(color: Cores.kTertiaryColor, width: 4.w),
+                      ),
+                      child: const Icon(
+                        Icons.arrow_back_ios_new,
+                        color: Cores.kTertiaryColor,
+                        size: 13,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.only(right: 50.w),
+                        child: Text(
+                          '${Strings.etapa} ${currentStep + 1}',
+                          style: TextStyle(
+                              fontSize: 40.sp,
+                              fontFamily: 'Roboto-Regular',
+                              fontWeight: FontWeight.w500),
+                          textAlign: TextAlign.center,
+                        ),
+                      ))
+                ],
+              ),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 20.h),
+                        height: 120.h,
+                        alignment: Alignment.center,
+                        child: Text(
+                          etapas[currentStep],
+                          style: TextStyle(
+                              fontSize: 60.sp,
+                              fontFamily: 'Roboto-Regular',
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      Container(
+                        height: 850.h,
+                        margin: EdgeInsets.only(top: 20.h, bottom: 20.h),
+                        child: Image.asset(
+                          Strings.pergunta1Asset,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20.h),
+                        decoration: BoxDecoration(
+                          border:
+                          Border.all(color: Cores.kTertiaryColor, width: 4.w),
+                          borderRadius: BorderRadius.circular(13.r),
+                        ),
+                        child: LinearProgressIndicator(
+                          value: progressoTotal,
+                          backgroundColor: Cores.kWhiteColor,
+                          valueColor: const AlwaysStoppedAnimation<Color>(
+                              Cores.kTertiaryColor),
+                        ),
+                      ),
+                      Text('${progressoTotal.toStringAsFixed(2)}% completo',
+                          style: TextStyle(
+                              fontSize: 33.sp,
+                              fontFamily: 'Roboto-Regular',
+                              fontWeight: FontWeight.w500,
+                              color: Cores.kTertiaryColor)),
+                      Container(
+                        margin: EdgeInsets.only(top: 20.h),
+                        height: 120.h,
+                        alignment: Alignment.center,
+                        child: Text(
+                          perguntas[currentStep][currentQuestion],
+                          style: TextStyle(
+                              fontSize: 60.sp,
+                              fontFamily: 'Roboto-Regular',
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20.h),
+                        child: Text(
+                          Strings.quantEstrelaGosta,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 45.sp, fontFamily: 'Roboto-Regular'),
+                        ),
+                      ),
+                      StarRate(
+                        selectedStar: selectedStar,
+                        selecionarEstrela: selecionarEstrela,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          registrarResposta();
+                        },
+                        child: Container(
+                          height: 120.h,
+                          width: 350.w,
+                          margin: EdgeInsets.only(top: 5.h, bottom: 5.h),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30.r),
+                              color: corBotaoNext),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Image(
+                                  image: AssetImage(Strings.proximoIconAsset)),
+                              SizedBox(
+                                width: 20.w,
+                              ),
+                              Text(
+                                Strings.proximo,
+                                style: TextStyle(
+                                  fontSize: 45.sp,
+                                  color: Cores.kWhiteColor,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+        ));
   }
 
   void registrarResposta() {
