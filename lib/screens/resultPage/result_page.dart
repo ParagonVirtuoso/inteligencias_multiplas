ddc9d96e1d38122d11f35356fc3e9d7264aae73e
Denys Fernandes <38758938+ParagonVirtuoso@users.noreply.github.com>
2023-04-16 18:34:42 -0300

    feature: criado item de resultado
    criado card item para o resultado.

--- lib/screens/resultPage/result_page.dart
+++ lib/screens/resultPage/result_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../utils/cores.dart';
+import '../../utils/strings.dart';
 
 class ResultPage extends StatefulWidget {
   const ResultPage({super.key});
@@ -16,6 +14,91 @@ class ResultPage extends StatefulWidget {
 class _ResultPageState extends State<ResultPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Cores.kAzulBotaoItemColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+          iconTheme: const IconThemeData(
+            color: Colors.white,
+          ),
+        ),
+        body: Container(
+            margin: EdgeInsets.only(
+                left: 50.w, right: 50.w, top: 10.h, bottom: 10.h),
+            child: Column(children: [
+              Container(
+                height: 380.h,
+                decoration: BoxDecoration(
+                    color: Cores.kWhiteColor,
+                    borderRadius: BorderRadius.circular(60.r),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 1,
+                        blurRadius: 2,
+                        offset: const Offset(0, 2),
+                      ),
+                    ]),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            'Musical',
+                            style: TextStyle(
+                                fontSize: 50.sp,
+                                color: Cores.kTertiaryColor,
+                                fontFamily: 'Roboto-Regular',
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            '50 pontos',
+                            style: TextStyle(
+                                fontSize: 45.sp,
+                                color: Cores.kDeepGreyColor,
+                                fontFamily: 'Roboto-Regular',
+                                fontWeight: FontWeight.w500),
+                          ),
+                          OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                backgroundColor: Colors.transparent,
+                                side: const BorderSide(
+                                    width: 0, color: Colors.transparent),
+                              ),
+                              onPressed: () {},
+                              child: Container(
+                                width: 250.w,
+                                height: 70.h,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                    color: Cores.kAzulBotaoItemColor,
+                                    borderRadius: BorderRadius.circular(20.r)),
+                                child: Text(
+                                  'Ver mais',
+                                  style: TextStyle(
+                                      fontSize: 38.sp,
+                                      color: Cores.kWhiteColor,
+                                      fontFamily: 'Roboto-Regular',
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ))
+                        ],
+                      ),
+                      SizedBox(
+                        width: 600.w,
+                        child: Image.asset(
+                          Strings.concluidoAsset,
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    ]),
+              )
+            ])));
   }
 }
