4b61401768e04ca32b4ce03e52c0d71debf7dde4
Denys Fernandes <38758938+ParagonVirtuoso@users.noreply.github.com>
2023-06-04 19:33:59 -0300

    feature: adicionado cargo ao developer item

--- lib/screens/developersPage/componentes/developer_item.dart
+++ lib/screens/developersPage/componentes/developer_item.dart
@@ -5,101 +5,145 @@ import '../../../utils/cores.dart';
 import '../../../utils/strings.dart';
 
 class DeveloperItem extends StatelessWidget {
-  const DeveloperItem({Key? key}) : super(key: key);
+  final String name;
+  final String email;
+  final String githubUrl;
+  final String linkedinUrl;
+  final String instagramUrl;
+  final String imageUrl;
+  final String cargo;
+
+  const DeveloperItem({
+    Key? key,
+    required this.name,
+    required this.email,
+    required this.githubUrl,
+    required this.linkedinUrl,
+    required this.instagramUrl,
+    required this.imageUrl,
+    required this.cargo,
+  }) : super(key: key);
 
   Future<void> _launchUrl(String url) async {
     if (!await launchUrl(Uri.parse(url))) {
-      throw Exception('Não foi possivel abrir : $url');
+      throw Exception('Não foi possível abrir: $url');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 220.w,
-          height: 220.h,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(100),
-            image: const DecorationImage(
-              image: NetworkImage(
-                  'https://firebasestorage.googleapis.com/v0/b/fury-db-ifmi.appspot.com/o/1684283591595-modified.png?alt=media&token=b56fd741-3e92-4e18-aa31-0c669fff9094'),
-              fit: BoxFit.contain,
-            ),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.only(left: 50.w),
-          height: 180.h,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 50.w, vertical: 30.h),
+      decoration: BoxDecoration(
+        color: Cores.kGreyColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          Row(
             children: [
-              Text(
-                'Denys Fernandes',
-                style: TextStyle(
-                  fontSize: 45.sp,
-                  fontWeight: FontWeight.bold,
+              Container(
+                width: 220.w,
+                height: 220.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  image: DecorationImage(
+                    image: NetworkImage(imageUrl),
+                    fit: BoxFit.contain,
+                  ),
                 ),
               ),
-              SizedBox(height: 10.h),
-              Text(
-                'Denysfernandes21@gmail.com',
-                style: TextStyle(fontSize: 38.sp, color: Cores.kDeepGreyColor),
-              ),
-              SizedBox(height: 10.h),
               Container(
-                  width: 400.w,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            _launchUrl('https://github.com/ParagonVirtuoso');
-                          },
-                          child: Container(
-                            width: 60.w,
-                            height: 60.h,
-                            child: Image.asset(
-                              Strings.githubIconAsset,
-                              fit: BoxFit.contain,
+                padding: EdgeInsets.only(left: 50.w),
+                height: 180.h,
+                width: 560.w,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      name,
+                      style: TextStyle(
+                        fontSize: 45.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10.h),
+                    Text(
+                      email,
+                      style: TextStyle(
+                        fontSize: 38.sp,
+                        color: Cores.kDeepGreyColor,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    SizedBox(height: 10.h),
+                    Container(
+                      width: 400.w,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              _launchUrl(githubUrl);
+                            },
+                            child: Container(
+                              width: 60.w,
+                              height: 60.h,
+                              child: Image.asset(
+                                Strings.githubIconAsset,
+                                fit: BoxFit.contain,
+                              ),
                             ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            _launchUrl(
-                                'https://www.linkedin.com/in/denys-fernandes-a1916198/');
-                          },
-                          child: Container(
-                            width: 60.w,
-                            height: 60.h,
-                            child: Image.asset(
-                              Strings.linkedinIconAsset,
-                              fit: BoxFit.contain,
+                          GestureDetector(
+                            onTap: () {
+                              _launchUrl(linkedinUrl);
+                            },
+                            child: Container(
+                              width: 60.w,
+                              height: 60.h,
+                              child: Image.asset(
+                                Strings.linkedinIconAsset,
+                                fit: BoxFit.contain,
+                              ),
                             ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            _launchUrl('https://www.instagram.com/denysfh/');
-                          },
-                          child: Container(
-                            width: 60.w,
-                            height: 60.h,
-                            child: Image.asset(
-                              Strings.instagramIconAsset,
-                              fit: BoxFit.contain,
+                          GestureDetector(
+                            onTap: () {
+                              _launchUrl(instagramUrl);
+                            },
+                            child: Container(
+                              width: 60.w,
+                              height: 60.h,
+                              child: Image.asset(
+                                Strings.instagramIconAsset,
+                                fit: BoxFit.contain,
+                              ),
                             ),
                           ),
-                        ),
-                      ]))
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
-        )
-      ],
+          Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                cargo,
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 32.sp,
+                  fontWeight: FontWeight.bold,
+                  color: Cores.kDeepGreyColor,
+                ),
+              )),
+        ],
+      ),
     );
   }
 }
--- lib/screens/developersPage/developers_page.dart
+++ lib/screens/developersPage/developers_page.dart
@@ -1,11 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../componentes/descriptor_content.dart';
-import '../../utils/cores.dart';
 import '../../utils/strings.dart';
-import '../homePage/componentes/drawer_custom.dart';
 import 'componentes/developer_item.dart';
 
 class DevelopersPage extends StatefulWidget {
@@ -16,19 +13,6 @@ class DevelopersPage extends StatefulWidget {
 }
 
 class _DevelopersPageState extends State<DevelopersPage> {
-  var _statusBarIcon = SystemUiOverlayStyle.light;
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  var _user = '';
-  var _profilePic = '';
-  var _email = '';
-
-  @override
-  initState() {
-    super.initState();
-    _user = _auth.currentUser?.displayName ?? '';
-    _profilePic = _auth.currentUser?.photoURL ?? '';
-    _email = _auth.currentUser?.email ?? '';
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +30,35 @@ class _DevelopersPageState extends State<DevelopersPage> {
             textAlign: TextAlign.center,
           ),
         ),
-        const DeveloperItem()
+        SizedBox(
+            height: 700.h,
+            child: const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  DeveloperItem(
+                    name: 'Denys Fernandes',
+                    email: 'denysfernandes21@gmail.com',
+                    githubUrl: 'https://github.com/ParagonVirtuoso',
+                    linkedinUrl:
+                        'https://www.linkedin.com/in/denys-fernandes-a1916198/',
+                    instagramUrl: 'https://www.instagram.com/denysfh/',
+                    imageUrl:
+                        'https://firebasestorage.googleapis.com/v0/b/fury-db-ifmi.appspot.com/o/1684283591595-modified.png?alt=media&token=b56fd741-3e92-4e18-aa31-0c669fff9094',
+                    cargo: 'Desenvolvedor',
+                  ),
+                  DeveloperItem(
+                    name: 'Elias Ribeiro',
+                    email: 'Elliasribeiro99@gmail.com',
+                    githubUrl: 'https://github.com/sailleribeiro',
+                    linkedinUrl:
+                        'https://www.linkedin.com/in/elias-ribeiro-b74649174/',
+                    instagramUrl: 'https://www.instagram.com/elliasribeiro_',
+                    imageUrl:
+                        'https://firebasestorage.googleapis.com/v0/b/fury-db-ifmi.appspot.com/o/perfil_eelias%20(2).png?alt=media&token=e2f6b9ca-4b43-4791-a2cc-9f9046be53cf',
+                    cargo: 'UI / UX Designer',
+                  )
+                ]))
       ]),
     );
   }
