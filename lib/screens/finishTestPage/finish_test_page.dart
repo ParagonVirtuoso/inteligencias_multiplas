10ca2b048346ef3bdd2a37036545c570265f2b3f
Denys Fernandes <38758938+ParagonVirtuoso@users.noreply.github.com>
2023-09-12 22:48:42 -0300

    feature: criado metodo para salvar as notas calculadas no firebase

--- lib/screens/finishTestPage/finish_test_page.dart
+++ lib/screens/finishTestPage/finish_test_page.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../controller/finishTestController/finish_test_controller.dart';
 import '../../utils/cores.dart';
 import '../../utils/strings.dart';
 
@@ -13,6 +16,15 @@ class FinishTest extends StatefulWidget {
 }
 
 class _FinishTestState extends State<FinishTest> {
+  dynamic respostas;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseDatabase database = FirebaseDatabase.instance;
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    respostas = ModalRoute.of(context)?.settings.arguments;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,7 +90,7 @@ class _FinishTestState extends State<FinishTest> {
                   height: 400.h,
                   child: OutlinedButton(
                       onPressed: () {
-                        Navigator.pushNamed(context, '/result');
+                        salvarResultado();
                       },
                       style: OutlinedButton.styleFrom(
                         backgroundColor: Colors.transparent,
@@ -119,5 +131,33 @@ class _FinishTestState extends State<FinishTest> {
             ],
           ),
         ));
+  }
+
+  void salvarResultado() {
+    var notas =
+        List.generate(7, (i) => List.generate(10, (j) => respostas[j][i]));
+    var total = {
+      'Cinestesica': notas[0].reduce((a, b) => a + b),
+      'Espacial': notas[1].reduce((a, b) => a + b),
+      'Interpessoal': notas[2].reduce((a, b) => a + b),
+      'Intrapessoal': notas[3].reduce((a, b) => a + b),
+      'Linguistica': notas[4].reduce((a, b) => a + b),
+      'Logico': notas[5].reduce((a, b) => a + b),
+      'Musical': notas[6].reduce((a, b) => a + b),
+    };
+    salvarTotalFirebase(total);
+    FinishResultController().navResult(context, total);
+  }
+
+  Future<void> salvarTotalFirebase(total) async {
+    if (_auth.currentUser == null) {
+      return;
+    }
+    await database
+        .ref()
+        .child('usuarios')
+        .child(_auth.currentUser!.uid)
+        .child('TOTAL')
+        .set(total);
   }
 }
